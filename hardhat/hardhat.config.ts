d705a1ac6b805fdfc39ecd2cd187c47665a6dfba
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2024-11-16 11:37:45 +0700

    Task utilities

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,9 +1,21 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import "dotenv/config";
+
+// Tasks
+import "./tasks";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
   defaultNetwork: "hardhat",
+  networks: {
+    // SOURCE: https://docs.base.org/docs/tools/hardhat/
+    "base-sepolia": {
+      url: "https://sepolia.base.org",
+      accounts: [process.env.PRIVATE_KEY as string],
+      gasPrice: 1000000000,
+    },
+  },
 };
 
 export default config;
